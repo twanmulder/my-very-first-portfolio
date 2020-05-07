f7d2c8b40dba3c35009953f3eec45c9a937bd012
TwanMulder <twan.mulder@gmail.com>
2020-05-07 22:13:26 +0200

    Added basic text

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <main>
-      <h1>HUTS</h1>
+    <main className="home">
+      <div className="home-text">
+        <h2 className="serif">Launch your business</h2>
+        <p>
+          Learn about how we can succesfully give a <strong>rocket-boost</strong> to your venture.
+        </p>
+        <Link to="/about" className="button">
+          Explore the possibilities
+        </Link>
+      </div>
     </main>
   );
 }
