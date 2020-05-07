2e8a956f1937731003fcdfb1c24f34a6c4a9a3e3
TwanMulder <twan.mulder@gmail.com>
2020-05-07 22:35:07 +0200

    Added animation to hero

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [show, setShow] = useState(false);
+  setTimeout(() => {
+    setShow(true);
+  }, 1500);
+
   return (
-    <main className="home">
+    <main className={show ? "home -show" : "home"}>
       <div className="home-text">
-        <h2 className="serif">Launch your business</h2>
-        <p>
+        <h2 className="serif -reveal">Launch your business</h2>
+        <p className="-reveal">
           Learn about how we can succesfully give a <strong>rocket-boost</strong> to your venture.
         </p>
-        <Link to="/about" className="button">
-          Explore the possibilities
-        </Link>
+        <div className="button-wrapper -reveal">
+          <Link to="/about" className="button">
+            Explore the possibilities
+          </Link>
+        </div>
+      </div>
+
+      <div class="starfield">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </main>
   );
